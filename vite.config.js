e0194d3eceec35546b35b3991d91582d6de48e40
Pops1028 <mikearreola1975@gmail.com>
2026-05-14 00:07:52 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,28 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-  registerType: "autoUpdate",
-  includeAssets: ["icon-192.png", "icon-512.png", "favicon.ico"],
-  manifest: {
-    name: "Dog Bones",
-    short_name: "DogBones",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#111111",
-    theme_color: "#111111",
-    icons: [
-      {
-        src: "/icon-192.png",
-        sizes: "192x192",
-        type: "image/png"
-      },
-      {
-        src: "/icon-512.png",
-        sizes: "512x512",
-        type: "image/png"
+      registerType: "autoUpdate",
+      includeAssets: ["icon-192.png", "icon-512.png"],
+      manifest: {
+        name: "Dog Bones",
+        short_name: "DogBones",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#111111",
+        theme_color: "#111111",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
       }
-    ]
-  }
-})
+    })
+  ]
+});
