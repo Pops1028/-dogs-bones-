68bc31e7b160f9c42e6b09d031ed62e0d61f0bb2
Pops1028 <mikearreola1975@gmail.com>
2026-05-13 22:51:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Dog Bones",
+        short_name: "DogBones",
+        display: "standalone",
+        start_url: "/",
+        background_color: "#111111",
+        theme_color: "#111111",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 });
