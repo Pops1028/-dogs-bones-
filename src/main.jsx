ef998a5cff8e339aceabe36fd34c4f39f12f11f3
Pops1028 <mikearreola1975@gmail.com>
2026-05-14 16:32:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { registerSW } from "virtual:pwa-register";
+import "./index.css";
 
 registerSW({
   immediate: true
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root"))
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
